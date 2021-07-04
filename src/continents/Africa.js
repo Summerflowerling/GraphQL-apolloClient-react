c6dc2f6cc9c58f3dcf73236d929057c6a11ccb25
yuling <yulinglin@protonmail.com>
2021-07-04 09:44:53 +0800

    refactor: Loop through the array inside the object named languages

---
+++ src/continents/Africa.js
@@ -0,0 +1,43 @@
+import { useQuery } from "@apollo/client";
+import { GET_CONTINENTS } from "../GetContinents";
+
+function GetAfrica() {
+    const { loading, error, data } = useQuery(GET_CONTINENTS);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error </p>;
+
+    const children = data.continents.map(item => (item))
+    const country = children[0].countries.map(item => {
+        const lan = item.languages.map(item => (item.name))
+        return (
+            <tr key={item.code}>
+                <td>{item.code}</td>
+                <td>{item.name}</td>
+                <td>{item.native}</td>
+                <td>{item.capital}</td>
+                <td>{item.currency}</td>
+                <td>{lan}</td>
+            </tr>
+        )
+    }
+    )
+
+    return (
+        <div>
+            <table id="africa">
+                <colgroup span="6"></colgroup>
+                <tr>
+                    <th>Code</th>
+                    <th>Name</th>
+                    <th>Native</th>
+                    <th>Capital</th>
+                    <th>Currency</th>
+                    <th>Languages</th>
+                </tr>
+                {country}
+            </table>
+        </div>
+    );
+}
+
+export default GetAfrica;
